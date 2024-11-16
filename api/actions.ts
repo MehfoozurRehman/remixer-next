7638afd6ab7d267166197db5d017a9a9b3b6d618
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-17 02:23:06 +0500

    Update actions.ts

--- api/actions.ts
+++ api/actions.ts
@@ -1,9 +1,24 @@
 "use server";
 
-import "server-only";
-
 import prisma from "@/lib/prisma";
 
-const actions = {};
+const actions = {
+  createTodo: async (data: { title: string; content: string }) => {
+    return await prisma.todo.create({
+      data,
+    });
+  },
+  updateTodo: async (id: string, data: { title: string; content: string }) => {
+    return await prisma.todo.update({
+      where: { id },
+      data,
+    });
+  },
+  deleteTodo: async (id: string) => {
+    return await prisma.todo.delete({
+      where: { id },
+    });
+  },
+};
 
 export default actions;
