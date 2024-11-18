322164dbe7d716f3a2af4b968df9bb9d91230868
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-18 09:47:02 +0500

    Update actions.ts

--- api/actions.ts
+++ api/actions.ts
@@ -1,24 +1,40 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
-const actions = {
-  createTodo: async (data: { title: string; content: string }) => {
-    return await prisma.todo.create({
-      data,
-    });
-  },
-  updateTodo: async (id: string, data: { title: string; content: string }) => {
-    return await prisma.todo.update({
-      where: { id },
-      data,
-    });
-  },
-  deleteTodo: async (id: string) => {
-    return await prisma.todo.delete({
-      where: { id },
-    });
-  },
+export const createTodo = async (data: FormData) => {
+  console.log(data);
+
+  const title = data.get("title") || "";
+  const content = data.get("content") || "";
+
+  await prisma.todo.create({
+    data: {
+      title: title.toString(),
+      content: content.toString(),
+    },
+  });
+
+  revalidatePath("/");
 };
 
-export default actions;
+export const updateTodoStatus = async (data: {
+  id: string;
+  completed: boolean;
+}) => {
+  await prisma.todo.update({
+    where: { id: data.id },
+    data: {
+      completed: data.completed,
+    },
+  });
+
+  revalidatePath("/");
+};
+
+export const deleteTodo = async (id: string) => {
+  return await prisma.todo.delete({
+    where: { id },
+  });
+};
