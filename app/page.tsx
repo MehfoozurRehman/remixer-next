e2ee2980b295a5f199910a8dfb455a99632e4e13
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-18 09:47:09 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Form from "next/form";
+import { Todo } from "./Todo";
+import { createTodo } from "@/api/actions";
 import queries from "@/api/queries";
 
 export default async function Page() {
   const totos = await queries.allTodos();
 
-  console.log(totos);
-  return <div>Page</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <ul>
+        {totos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+
+      <Form action={createTodo}>
+        <input name="title" placeholder="Title" />
+        <textarea name="content" placeholder="Content" />
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
 }
